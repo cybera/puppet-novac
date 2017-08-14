340a684be0d6b519815502a84c8dfb27dcfb68c4
Cybera Sysadmin <sysadmin@cybera.ca>
2017-08-14 11:46:46 -0600

    Xenial version changes for Gems

--- manifests/params.pp
+++ manifests/params.pp
@@ -44,6 +44,26 @@ class novac::params {
         'ruby-dev'           => 'present',
       }
     }
+
+    'xenial': {
+      $gems = {
+        'amq-protocol'   => '2.2.0',
+        'amqp'           => '1.7.0',
+        'json'           => '1.8.3',
+        'mysql2'         => 'present',
+        'terminal-table' => 'present',
+        'eventmachine'   => 'present',
+        'sequel'         => 'present',
+        'inifile'        => 'present',
+        'parallel'       => 'present',
+      }
+
+      $packages = {
+        'libmysqlclient-dev' => 'present',
+        'build-essential'    => 'present',
+        'ruby-dev'           => 'present',
+      }
+    }
   }
 
 }
